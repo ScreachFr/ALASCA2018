4f28a9344c73ee54c7eeb21570e217fbea00c3cb
Alexandre GC <a.gaspardcilia@gmail.com>
2017-10-19 19:44:42 +0200

    J'avais oublié de créer le port offert.

--- fr/upmc/gaspardleo/step1/step12/cvm/CVMComponent.java
+++ fr/upmc/gaspardleo/step1/step12/cvm/CVMComponent.java
@@ -4,13 +4,23 @@ import fr.upmc.components.AbstractComponent;
 import fr.upmc.components.ComponentI;
 import fr.upmc.components.cvm.AbstractCVM;
 import fr.upmc.gaspardleo.step1.step12.cvm.interfaces.CVMI;
+import fr.upmc.gaspardleo.step1.step12.cvm.ports.CVMInboundPort;
 
 public class CVMComponent extends AbstractComponent implements CVMI {
 	private AbstractCVM realCVM;
 	
-	public CVMComponent(AbstractCVM realCVM) {
+	private CVMInboundPort cvmip;
+	
+	public CVMComponent(AbstractCVM realCVM, String cvmipUri) throws Exception {
 		super(1, 1);
 		this.realCVM = realCVM;
+		
+		this.cvmip = new CVMInboundPort(cvmipUri, this);
+		this.addPort(cvmip);
+		this.cvmip.publishPort();
+		
+		
+		this.addOfferedInterface(CVMI.class);
 	}
 	
 	@Override
