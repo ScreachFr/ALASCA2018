c1bab07db76cb3491f3b10c00fd9d51f3b7a39cf
leor <leo@leonor.com>
2018-02-03 16:13:37 +0100

    [+] doc du package classfactory

--- fr/upmc/gaspardleo/classfactory/ClassFactory.java
+++ fr/upmc/gaspardleo/classfactory/ClassFactory.java
@@ -7,10 +7,22 @@ import javassist.CtClass;
 import javassist.CtMethod;
 import javassist.NotFoundException;
 
+/**
+ * La classe <code> ClassFactory </ code> permet la création connector via Javassist
+ * @author Leonor & Alexandre
+ */
 public class ClassFactory {
 	
+	/** compteur pour la construction des noms uniques des connecteurs */
 	private static int id = 0;
 	
+	/**
+	 * Crée un nouveau connecteur à partir de l'interface spécifiée en paramètre
+	 * @param implementedInterface		Interface implémentée par le connecteur
+	 * @return							Un connecteur
+	 * @throws NotFoundException
+	 * @throws CannotCompileException
+	 */
 	public static Class<?> newConnector(Class<?> implementedInterface) 
 			throws NotFoundException, CannotCompileException {
 		
@@ -18,6 +30,15 @@ public class ClassFactory {
 				AbstractConnector.class, implementedInterface);
 	}
 	
+	/**
+	 * Crée un nouveau connecteur à partir d'un nom, de la classe qui l'étend et de l'interface qu'il implémente
+	 * @param connectorName			Nom du connecteur
+	 * @param extendedClass			Classes étendues par le connecteur soit AbstractConnector
+	 * @param implementedInterface	Interface implémentée par le connecteur
+	 * @return						Un connecteur
+	 * @throws NotFoundException
+	 * @throws CannotCompileException
+	 */
 	public static Class<?> newConnector(String connectorName, Class<?> extendedClass, Class<?> implementedInterface) throws NotFoundException, CannotCompileException {
 		
 		ClassPool cpool = ClassPool.getDefault();
@@ -58,6 +79,14 @@ public class ClassFactory {
 		return result.toClass();
 	}
 	
+	/**
+	 * Construit l'implémentation d'une méthode de l'interface qu'il implément grâce au nom de la méthode 
+	 * en question est du nombre de parèmtres qu'elle utilise
+	 * @param implementedInterface	Interface implémentée par le connecteur
+	 * @param methodName			Nom de la méthode qu'il doit implémenter
+	 * @param argCount				Nombre d'arguments de la méthode
+	 * @return						l'implémentation de la méthode par le connecteur
+	 */
 	private static String getConnectorMethodBody(Class<?> implementedInterface, String methodName, int argCount) {
 		
 		String result = "return ((" + implementedInterface.getCanonicalName() + ")this.offering)." + methodName + "(";
