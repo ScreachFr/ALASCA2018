17fea5e36545970686ce44c68d4c9d8500326c88
Alexandre GC <a.gaspardcilia@gmail.com>
2018-02-04 14:33:31 +0100

    [+] Doc

--- fr/upmc/gaspardleo/computerpool/ComputerPoolNetworkMaster.java
+++ fr/upmc/gaspardleo/computerpool/ComputerPoolNetworkMaster.java
@@ -6,17 +6,36 @@ import fr.upmc.components.AbstractComponent;
 import fr.upmc.gaspardleo.computerpool.interfaces.ComputerPoolNetworkMasterI;
 import fr.upmc.gaspardleo.computerpool.ports.ComputerPoolNetworkMasterInboundPort;
 
+/**
+ * La classe <code>ComputerPoolNetworkMaster</ code> implémente le composant représentant 
+ * un registre de <code>ComputerPool</code>.
+ * 
+ * <p><strong>Description</strong></p>
+ * Ce composant permet de disposer d'une liste de <code>ComputerPool</code> afin que le <code>PerformanceRegulator</code>
+ * puisse choisir celui qui lui convient.
+ * 
+ * @author Leonor & Alexandre
+ */
 public class ComputerPoolNetworkMaster 
 	extends AbstractComponent
 	implements ComputerPoolNetworkMasterI{
 	
+	// URI du composant.
 	private String uri;
 	
+	// Registe de ComputerPool. <URI du ComputerPool, URI du port ComputerPoolI in>
 	private HashMap<String, String> pools;
 	
-	
+	// Port serveur du ComputerPoolNetwordMasterI.
 	private ComputerPoolNetworkMasterInboundPort cpnmip;
 	
+	/**
+	 * @param componentUri
+	 * 		URI du composant.
+	 * @param computerPoolInboundPortUri
+	 * 		URI du port ComputerPoolNetworkMaster in.
+	 * @throws Exception
+	 */
 	public ComputerPoolNetworkMaster(String componentUri, String computerPoolInboundPortUri) throws Exception {
 		super(1, 1);
 		
@@ -36,17 +55,26 @@ public class ComputerPoolNetworkMaster
 		this.logMessage(uri + " has been created!");
 	}
 
+	/**
+	 * @see fr.upmc.gaspardleo.computerpool.interfaces.ComputerPoolNetworkMasterI#registerComputerPool(String, String)
+	 */
 	@Override
 	public void registerComputerPool(String computerPoolUri, String compterPoolInboundPortUri) {
 		pools.put(computerPoolUri, compterPoolInboundPortUri);
 		
 	}
 
+	/**
+	 * @see fr.upmc.gaspardleo.computerpool.interfaces.ComputerPoolNetworkMasterI#unregisterComputerPool(String)
+	 */
 	@Override
 	public void unregisterComputerPool(String computerPoolUri) {
 		pools.remove(computerPoolUri);
 	}
 
+	/**
+	 * @see fr.upmc.gaspardleo.computerpool.interfaces.ComputerPoolNetworkMasterI#getAvailableComputerPools()
+	 */
 	@Override
 	public HashMap<String, String> getAvailableComputerPools() {
 		return pools;
--- fr/upmc/gaspardleo/computerpool/interfaces/ComputerPoolNetworkMasterI.java
+++ fr/upmc/gaspardleo/computerpool/interfaces/ComputerPoolNetworkMasterI.java
@@ -2,12 +2,38 @@ package fr.upmc.gaspardleo.computerpool.interfaces;
 
 import java.util.HashMap;
 
+/**
+ * Cette interface représente le centre nerveux d'un réseau de <code>ComputerPool</code>. 
+ * En effet il permet de tenir un registre de <code>ComputerPool</code> disponible dans le système.
+ * 
+ * @author Leonor & Alexandre
+ */
 public interface ComputerPoolNetworkMasterI {
 	
+	/**
+	 * Ajoute un <code>ComputerPool</code> au registre.
+	 * @param computerPoolUri
+	 * 		URI du <code>ComputerPool</code>.
+	 * @param compterPoolInboundPortUri
+	 * 		URI du port <code>ComputerPoolI</code> in.
+	 * @throws Exception
+	 */
 	public void registerComputerPool(String computerPoolUri, String compterPoolInboundPortUri) throws Exception;
 	
+	/**
+	 * Supprime une entrée du registre de <code>ComputerPool</code>.
+	 * @param computerPoolUri
+	 * 		Entrée à enlever.
+	 * @throws Exception
+	 */
 	public void unregisterComputerPool(String computerPoolUri) throws Exception;
 	
+	/**
+	 * Retourne une liste de <code>ComputerPool</code> disponible.
+	 * @return
+	 * 		Map de <code>ComputerPool</code> qui contient en clef l'URI du <code>ComputerPool</code> et en valeur l'URI de sont port <code>ComputerPoolI</code> in.
+	 * @throws Exception
+	 */
 	public HashMap<String, String> getAvailableComputerPools() throws Exception;
 	
 }
